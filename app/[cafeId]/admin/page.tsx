e9a36a2e5a4aa2e635d37d5e422fea3d296bdcee
Piyush123-cmd <patilpiyush1820@gmail.com>
2026-09-23 11:49:02 +0530

    Fixed syntax and route code

--- app/[cafeId]/admin/page.tsx
+++ app/[cafeId]/admin/page.tsx
@@ -1,3 +1,8 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
+
 'use client';
 
 import React, { useState, useEffect } from 'react';
